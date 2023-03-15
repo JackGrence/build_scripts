5d1a461bae2749feba4c84635e7d36bc50381ede
JackGrence <green1181772@gmail.com>
2023-03-15 18:31:44 +0800

    Add python

---
+++ python/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir -p ~/code
+cd ~/code
+wget https://www.python.org/ftp/python/3.11.2/Python-3.11.2.tgz
+tar xzf Python-3.11.2.tgz
+cd Python-3.11.2
+
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+       libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+       libncurses5-dev libncursesw5-dev xz-utils tk-dev
+
+mkdir -p ~/build
+./configure --enable-optimizations --with-ensurepip=install --prefix=$HOME/build
+
+make -j
+make install
+ln -s ~/build/bin/python3.11 ~/.local/bin/python3
+ln -s ~/build/bin/pip3.11 ~/.local/bin/pip3
